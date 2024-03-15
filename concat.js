7cfe46d33e58319431d96c55c252fe99b2681358
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-15 19:00:21 -0300

    concat

---
+++ concat.js
@@ -0,0 +1,26 @@
+//concat
+
+//ex. 01
+
+//let cores se refere a uma lista de cores
+let cores = ['azul', 'bordo', 'ciano'];
+
+//let tons vai se referir a ordem alfabética das cores
+let tons = [1, 2 ,3 ];
+
+//essa let é para mesclarmos os dois arrays que estão nas lets 
+let coresAlfabética = cores.concat(tons);
+
+//exibimos tudo em um só array
+console.log(coresAlfabética);
+
+//ex. 02
+
+//
+let meninos = ['kevin', 'anna'];
+
+let meninas = ['lucas', 'carine'];
+
+let paresDança = meninos.concat(meninas);
+
+console.log(paresDança);
