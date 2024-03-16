00e5551bf08d191fd2d5f80d9122e7c4b8d1ab6a
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-16 11:00:22 -0300

    join.js

---
+++ join.js
@@ -0,0 +1,19 @@
+//join
+
+//ex. 01
+
+//let redesSociais (array)
+let redesSociais = ['whatsapp', 'instagram' ,'tiktok' ,'twitter', 'facebook'];
+
+//utilizei o console para exibir a let redesSociais, só que com o join, que transforma uma array em string, nesse primeiro exemplo deixei a separação padrão dele (,).
+console.log(redesSociais.join());
+
+//ex. 02
+
+//let hero (array)
+let hero = ['pantera negra', 'homem aranha', 'batman'];
+
+//aqui já usei o join com uma separação da minha escolha "vs.", ele transformou todos os elementos de um array em string, e separou como eu coloquei dentro dos parenteses do join (aqui:)).
+console.log(hero.join(" vs. "));
+
+//O método join() junta todos os elementos de um array em uma string e retorna esta string.
