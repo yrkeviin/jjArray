8f745fef965cad60be65c26f5d026b368e1717a9
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-16 08:46:02 -0300

    includes completo

--- includes.js
+++ includes.js
@@ -7,25 +7,35 @@
 //listaConvidados se refere aos convidados de uma festa.
 let listaConvidados = ['kevin', 'anna', 'lucca', 'sophia'];
 
-//essa mensagem será exibida apenas para o true || false não ficarem sozinhos na hora de serem exibidos.
-let mensagem01 = "O convidado está na lista? ="
+//criei uma variável usando o includes para facilitar a exibição no if, se alguém quiser saber se o convidado está ou não na lista e também na posição, basta a pessoa trocar o convidado que esta entre parenteses para algum outro, depois disso vai exibir se o convidado está ou não na lista e na posição inserida.
+let pessoa = listaConvidados.includes('anna', 1);
 
-console.log('-----LISTA DE CONVIDADOS!-----');
-//mensagem01 (apenas decorativo), listaConvidados.includes(nome do convidado que quero checar na lista), se o nome dentro do parentes estiver dentro da let listaConvidados, true, senão, false.
-console.log(mensagem01 ,listaConvidados.includes("anna"));
+console.log("-----LISTA DE CONVIDADOS!-----");
+
+//o if diz que se a let pessoa for true, vai exibir a mensagem afirmando a pessoa e a posição dela na lista, já o else, ele nega a posição ou o convidado (false).
+if (pessoa){
+    console.log('Sim! Esse convidado está na lista e na posição inserida!');
+}else{
+    console.log('Não! Esse convidado não está na lista ou não está na posição inserida!');
+}
 
 //ex.02
 
 //checar itens
 
 //listaCompra se refere à lista de compras.
-let listaCompra = ['danone', 'pipoca', 'suco', 'shampoo', 'sabonete', 'escova'];
+let listaCompra = ["danone", "pipoca", "suco", "shampoo", "sabonete", "escova"];
 
-//essa mensagem será exibida apenas para o true || false não ficarem sozinhos na hora de serem exibidos.
-let mensagem02 = "O item está na lista? ="
+//criei uma variável usando o includes para facilitar a exibição no if, se alguém quiser saber se o item está ou não na lista, basta a pessoa trocar o item que esta entre parenteses para algum outro, depois disso vai exibir se o item está ou não na lista.
+let item = listaCompra.includes('manga');
 
-console.log('-----LISTA DE COMPRAS-----');
-//mensagem02 (apenas decorativo), listaCompra.includes(item que eu quero checar na lista), 2 (posição que o elemento está) se a palavra que estiver dentro do parenteses da let listaCompra && na posição correta, true, senão, false.
-console.log(mensagem02, listaCompra.includes("suco", 2));
+console.log("-----LISTA DE COMPRAS-----");
 
-//O método includes() determina se um array contém um determinado elemento, retornando true ou false, lembrando que se você colocar ou ele estiver na posição errada, ele vai retornar false, já se estiver na certa e o elemento aparecer na lista, true.
+//esse if vai exibir se o let item está presente na lista de compras, se ele estiver, vai exibir a mensagem dizendo que o item exista na lista, senão, ele vai exibir a mensagem dizendo que não tem, mesma coisa que o true e false, mas com palavras diferentes!
+if (item){
+    console.log('Sim! Esse item está na sua lista de compras!');
+}else{
+    console.log('Não! Esse item não está na sua lista!');
+}
+
+//O método includes() determina se um array contém um determinado elemento, retornando true ou false, lembrando que se você colocar ou ele estiver na posição errada (caso você determine posição), ele vai retornar false, já se estiver na posição certa e o elemento estiver presente no array, true.
