62708906bf41c436cbd7ac3f99301da48f2a183f
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-16 10:58:48 -0300

    adicionei pontuação final nos comentarios

--- isArray.js
+++ isArray.js
@@ -2,13 +2,13 @@
 
 //ex. 01
 
-//let  numero (number)
+//let  numero (number).
 let number = 23;
 
-//let color (array)
+//let color (array).
 let color = ['black', 'green', 'gray'];
 
-//let str (string)
+//let str (string).
 let str = 'Cada um com a sua sorte';
 
 //ele checa se a let é ou não uma array, retorna true ou false.
@@ -18,19 +18,19 @@ console.log(Array.isArray(color));
 
 //nesse ex fiz um pouco mais complexo.
 
-//let jogos (array)
+//let jogos (array).
 let jogos = ['valorant', 'gta', 'minecraft', 'rocket league'];
 
-//let marcas (string)
+//let marcas (string).
 let marcas = 'nike, adidas, lacoste';
 
-//let numero (number)
+//let numero (number).
 let numero = 20;
 
-//coloquei essa let para checar se tal let era ou não uma array
+//coloquei essa let para checar se tal let era ou não uma array.
 let check = Array.isArray(marcas);
 
-//esse if vai exibir afirmando se é uma array caso o let check seja true, se for false ele fica com o else, que exibe que não é uma array
+//esse if vai exibir afirmando se é uma array caso o let check seja true, se for false ele fica com o else, que exibe que não é uma array.
 if (check){
     console.log('É um array!');
 }else{
