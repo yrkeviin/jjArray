7830ba233274c555d7ada1db4712a444fad1d709
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-17 21:42:42 -0300

    unshift

---
+++ unshift.js
@@ -0,0 +1,23 @@
+//unshift
+
+//ex. 01
+
+//1 ao 3.
+let numeros = [1, 2, 3];
+
+//utilizei o unshift para adicionar o número 0 na posição zero do array.
+let adicionarNumero = numeros.unshift(0);
+
+//exibi.
+console.log(numeros);
+
+//ex. 02
+
+//nomes.
+let nomes = ['andressa', 'bia', 'carlos' ,'danilo'];
+
+//utilizei o unshift para adicionar o elemento 'anna' na primeira posição do array.
+let adicionarNome = nomes.unshift('ana');
+
+//exibi.
+console.log(nomes);
