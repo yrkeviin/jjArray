e677da67ebf2df0f5e4208c09e71a0d244c69d62
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-17 21:49:20 -0300

    unshift

--- unshift.js
+++ unshift.js
@@ -14,10 +14,15 @@ console.log(numeros);
 //ex. 02
 
 //nomes.
-let nomes = ['andressa', 'bia', 'carlos' ,'danilo'];
+let nomes = ['andressa', 'bia'];
 
-//utilizei o unshift para adicionar o elemento 'anna' na primeira posição do array.
-let adicionarNome = nomes.unshift('ana');
+//utilizei o unshift para adicionar os elemento 'agnes', 'amanda', 'ana'. Eles vão ser adicionados na ordem que os escrevi ex. escrevi primero o nome agnes, então, agnes passa a ser a primeira posição do array (que no caso é 0).
+let adicionarNome = nomes.unshift('agnes', 'amanda', 'ana');
 
 //exibi.
 console.log(nomes);
+//usei o lenght para mostrar que realmente adicionou e já atualizou a quantidade de elementos que eu coloquei dentro dos parenteses do unshift.
+console.log(nomes.length);
+
+//O método unshift() adiciona um ou mais elementos no início de um array e retorna o número de elementos (propriedade length) atualizado.
+//A propriedade length especifíca o número de elementos em um array.
