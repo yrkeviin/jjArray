ba3a073d1128be745352b0d1932f2b37fa7df264
yrkeviin <kevinlima.tds1.senai@gmail.com>
2024-03-17 21:33:03 -0300

    reverse

---
+++ reverse.js
@@ -0,0 +1,17 @@
+//reverse
+
+//ex. 01
+
+let crescente = [1, 2, 3, 4, 5];
+
+let decrescente = crescente.reverse();
+
+console.log(decrescente);
+
+//ex. 02
+
+let ordemAlfabetica = ['a', 'b', 'c', 'd'];
+
+let alfabeticaInvertida = ordemAlfabetica.reverse();
+
+console.log(alfabeticaInvertida);
